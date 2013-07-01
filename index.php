96aec24a7a4969bea96007745e68812dede7802b
Hamid Paskamari <hpaskamari@gmail.com>
2013-07-01 15:13:04 +0430

    update

--- index.php
+++ index.php
@@ -3,51 +3,49 @@
 <head>
 	<meta charset="UTF-8" />
 	<title>W3project</title>
+	<link rel="stylesheet" type="text/css" href="library/1styles.css">
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<script type="text/javascript" src="library/jquery-1.9.1.min.js"></script>
 </head>
 <body>
 
+	<?php
+		include('library/lib.php');
+		$imgs = getFiles('images/Slider','jpg','png');
+	?>
 
 	<div class="container">
 
-		<div class="top">
-			
+		<div class="top"></div>
 
-
-			
-
-		</div>
-
-		<div class="bottom">
+		<div class="bottom cover">
+			<div class="prev"></div>
+		    <div class="next"></div>
 
 			<div class="slideshow">
 				<div class="train">
-					  <?php
-					    include('library/lib.php');
-					    $imgs = getFiles('images','jpg','png');
-					    foreach($imgs as $img){
-					    	$temp = explode('.',$img);
+					<?php
+						foreach($imgs as $img){
 
-					      	echo "
-					      			<div>
-					      				<div>
-					      					<img src='images/$img' />
-					      				</div>
-					      				<div>
-					    					<h2>'$temp[1]'</h2>
-					      				</div>
-					      			</div>
-					      		";
-					    }
-					  ?>
+							$temp = explode('.',$img);
+
+							echo "
+								<div class='local-train'>
+									<div class='logo'>
+										<img src='images/Slider/$img' />
+									</div>
+									<div class='subject-logo'>
+										<h1>$temp[1]</h1>
+										<p class='description'>the international sales	and  headquarters.</p>
+									</div>
+								</div>
+							";
+						}
+					?>
 				</div>
+				
+		    </div>
 
-				<div class="nav">
-					<ul class="btns"></ul>
-				</div>
-
-			</div>
 
 		</div>
 		<div class="pointer"></div>
@@ -55,4 +53,4 @@
 
 	<script type="text/javascript" src="script.js"></script>
 </body>
-</html>	
+</html>
